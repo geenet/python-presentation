5f5b37d15e38038d1789f27286d9edaf9ae5f2d2
a <a@a.(none)>
2014-01-31 11:47:15 +0100

    improved processing script

--- introduction/multi-processing.py
+++ introduction/multi-processing.py
@@ -1,15 +1,25 @@
+import os,multiprocessing
+from time import sleep
 
-import multiprocessing
-newlist = multiprocessing.Manager().list()
-def addto(val):
-    newlist.append(val)
+result_list = multiprocessing.Manager().list()
 
-pool = multiprocessing.Pool()
-for i in xrange(10):
-    pool.apply_async(addto, (i,))
+def process_square(num):
+    pid = os.getpid()
+    print('PPID:%s - PID:%s - NUM:%s - begin'%(os.getppid(), pid, str(num)))
+
+    square = num * num
+    sleep(2)
+   
+    print('PPID:%s - PID:%s - NUM:%s - SQUARE = %s'%(os.getppid(), pid, str(num), str(square) ) )
+    result_list.append( {"PID %s"%pid: square} )
+
+pool = multiprocessing.Pool(4) # 4 workers
+for i in xrange(10): # 10 jobs
+    pool.apply_async(process_square, (i,))
 pool.close()
 pool.join()
-print(newlist)
+print(result_list)
 
-# save code to multi-processing.py
-# python multi-processing.py
+# check running processes
+# ps -o pid,ppid,cmd ax | grep -i "[m]ulti-processing.py"
+
